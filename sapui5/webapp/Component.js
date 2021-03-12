ed2bb10b312d175f3d3aad0431c8f60807571819
Chandra Dutt <chandradutt.chandu@gmail.com>
2021-03-12 08:48:53 +0000

    Retrofit till Step-14

--- sapui5/webapp/Component.js
+++ sapui5/webapp/Component.js
@@ -1,24 +1,28 @@
 sap.ui.define([
-   "sap/ui/core/UIComponent",
-   "sap/ui/model/json/JSONModel"
-], function (UIComponent,JSONModel) {
-   "use strict";
-   return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
-      metadata : {
-            manifest: "json"
-      },
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel"
+], function (UIComponent, JSONModel) {
+	"use strict";
 
-      init : function () {
-         // call the init function of the parent
-         UIComponent.prototype.init.apply(this, arguments);
-         // set data model
-         var oData = {
-            recipient : {
-               name : "World"
-            }
-         };
-         var oModel = new JSONModel(oData);
-         this.setModel(oModel);
-      }
-   });
+	return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
+
+		metadata : {
+			manifest: "json"
+		},
+
+		init : function () {
+			// call the init function of the parent
+			UIComponent.prototype.init.apply(this, arguments);
+
+			// set data model
+			var oData = {
+				recipient : {
+					name : "World"
+				}
+			};
+			var oModel = new JSONModel(oData);
+			this.setModel(oModel);
+		}
+	});
+
 });
